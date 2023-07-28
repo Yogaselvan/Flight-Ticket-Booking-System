2d0c6cf2ee8e4231ac88079c569619d63afe58f5
YOGASELVAN S <yogaselvansaravanan557@gmail.com>
2023-07-28 12:59:59 +0530

    Flight

--- MicroServices/Server/feedbackservice/repository/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.CustomerSupport;
-
-public interface CustomerRepo extends JpaRepository<CustomerSupport, Integer>{
-
-}
